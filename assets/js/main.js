9c81afbb4732ec2264598e15923a0429747bdd03
Yi Ching Wang <doo8409255@gmail.com>
2022-09-24 13:56:07 +0800

    0924

--- assets/js/main.js
+++ assets/js/main.js
@@ -233,4 +233,18 @@ document.addEventListener('DOMContentLoaded', () => {
     aos_init();
   });
 
+
+  $(document).ready(function () {
+    $(".accordion-title").click(function (e) {
+      var accordionitem = $(this).attr("data-tab");
+      $("#" + accordionitem).slideToggle().parent().siblings().find(".accordion-content").slideUp();
+
+      $(this).toggleClass("active-title");
+      $("#" + accordionitem).parent().siblings().find(".accordion-title").removeClass("active-title");
+
+      $("i.fa-chevron-down", this).toggleClass("chevron-top");
+      $("#" + accordionitem).parent().siblings().find(".accordion-title i.fa-chevron-down").removeClass("chevron-top");
+    });
+
+  });
 });
